69c1b550415f42a22f5d0ddab65b454d37be777c
AnnaSzczech <anna.szczech89@gmail.com>
2022-09-05 00:04:23 +0200

    ScoreBoard test cases fixed

--- score-board/src/main/java/football/world/cup/score/board/model/ScoreBoard.java
+++ score-board/src/main/java/football/world/cup/score/board/model/ScoreBoard.java
@@ -1,2 +1,48 @@
-package football.world.cup.score.board.model;public class ScoreBoard {
+package football.world.cup.score.board.model;
+
+public class ScoreBoard {
+    private String homeTeam;
+    private int homeTeamScore;
+    private String awayTeam;
+    private int awayTeamScore;
+
+    public void setBoard(final String homeTeam, final String awayTeam) {
+        // do nothing
+    }
+
+    public void resetBoard() {
+        // do nothing
+    }
+
+    public String getHomeTeam() {
+        return homeTeam;
+    }
+
+    public int getHomeTeamScore() {
+        return homeTeamScore;
+    }
+
+    public void setHomeTeamScore(final int homeTeamScore) {
+        this.homeTeamScore = homeTeamScore;
+    }
+
+    public String getAwayTeam() {
+        return awayTeam;
+    }
+
+    public int getAwayTeamScore() {
+        return awayTeamScore;
+    }
+
+    public void setAwayTeamScore(final int awayTeamScore) {
+        this.awayTeamScore = awayTeamScore;
+    }
+
+    private void setHomeTeam(final String homeTeam) {
+        this.homeTeam = homeTeam;
+    }
+
+    private void setAwayTeam(final String awayTeam) {
+        this.awayTeam = awayTeam;
+    }
 }
--- score-board/src/test/java/football/world/cup/score/board/model/ScoreBoardTestSuite.java
+++ score-board/src/test/java/football/world/cup/score/board/model/ScoreBoardTestSuite.java
@@ -1,2 +1,52 @@
-package football.world.cup.score.board.model;public class ScoreBoardTestSuite {
+package football.world.cup.score.board.model;
+
+import org.junit.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ScoreBoardTestSuite {
+    private static final Logger LOG = LoggerFactory.getLogger(ScoreBoardTestSuite.class);
+    private static int testCounter = 0;
+
+    @BeforeClass
+    public static void beforeAllTests() {
+        LOG.debug("This is the beginning of tests.");
+    }
+
+    @AfterClass
+    public static void afterAllTests() {
+        LOG.debug("All tests are finished.");
+    }
+
+    @Before
+    public void beforeEveryTest() {
+        testCounter++;
+        LOG.debug("Preparing to execute test #" + testCounter);
+    }
+
+    @Test
+    public void testSetBoard() {
+        //Given
+        final ScoreBoard scoreBoard = new ScoreBoard();
+
+        //When
+        scoreBoard.setBoard("Mexico", "Canada");
+
+        //Then
+        Assert.assertNotNull(scoreBoard.getHomeTeam());
+        Assert.assertNotNull(scoreBoard.getAwayTeam());
+    }
+
+    @Test
+    public void testResetBoard() {
+        //Given
+        final ScoreBoard scoreBoard = new ScoreBoard();
+
+        //When
+        scoreBoard.resetBoard();
+
+        //Then
+        Assert.assertNull(scoreBoard.getHomeTeam());
+        Assert.assertNull(scoreBoard.getAwayTeam());
+    }
 }
