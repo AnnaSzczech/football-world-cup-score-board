66c9b128d143445346cd9ab0594d58d689861d44
AnnaSzczech <anna.szczech89@gmail.com>
2022-09-05 00:31:45 +0200

    Condition added to startGame

--- score-board/src/main/java/football/world/cup/score/board/api/FootballWorldCupScoreBoard.java
+++ score-board/src/main/java/football/world/cup/score/board/api/FootballWorldCupScoreBoard.java
@@ -1,28 +1,48 @@
 package football.world.cup.score.board.api;
 
+import football.world.cup.score.board.model.ScoreBoard;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.apache.commons.lang3.StringUtils.trim;
+
 public class FootballWorldCupScoreBoard implements IFootballWorldCupScoreBoard {
 
-    private boolean isGameContinues;
     private static final Logger LOG = LoggerFactory.getLogger(FootballWorldCupScoreBoard.class);
 
-    @Override
-    public void startGame(String homeTeam, String awayTeam) {
-        this.isGameContinues = true;
-    }
+    private boolean isGameContinues;
+    private final ScoreBoard scoreBoard;
 
-    @Override
-    public void finishGame() {
+    public FootballWorldCupScoreBoard(final ScoreBoard scoreBoard) {
+        this.scoreBoard = scoreBoard;
         this.isGameContinues = false;
     }
 
     @Override
-    public void updateScore(String score) {
+    public void startGame(final String homeTeam, final String awayTeam) {
+        if (!isGameContinues) {
+            scoreBoard.setBoard(trim(homeTeam), trim(awayTeam));
+            this.isGameContinues = true;
+        } else {
+            LOG.error("Game has not been started!");
+        }
+    }
+
+    @Override
+    public void finishGame() {
+        if (isGameContinues) {
+            scoreBoard.resetBoard();
+            this.isGameContinues = false;
+        } else {
+            LOG.error("Game has not been finished!");
+        }
+    }
+
+    @Override
+    public void updateScore(final String score) {
         // do nothing
     }
 
--- score-board/src/test/java/football/world/cup/score/board/api/FootballWorldCupScoreBoardTestSuite.java
+++ score-board/src/test/java/football/world/cup/score/board/api/FootballWorldCupScoreBoardTestSuite.java
@@ -1,5 +1,6 @@
 package football.world.cup.score.board.api;
 
+import football.world.cup.score.board.model.ScoreBoard;
 import org.junit.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,31 +28,41 @@ public class FootballWorldCupScoreBoardTestSuite {
     @Test
     public void testStartGame() {
         //Given
-        FootballWorldCupScoreBoard footballWorldCupScoreBoard = new FootballWorldCupScoreBoard();
+        final ScoreBoard scoreBoard = new ScoreBoard();
+        FootballWorldCupScoreBoard footballWorldCupScoreBoard = new FootballWorldCupScoreBoard(scoreBoard);
 
         //When
-        footballWorldCupScoreBoard.startGame("Mexico", "Canada");
+        footballWorldCupScoreBoard.startGame("Mexico ", " Canada ");
 
         //Then
         Assert.assertTrue(footballWorldCupScoreBoard.isGameContinues());
+        Assert.assertEquals("Mexico", scoreBoard.getHomeTeam());
+        Assert.assertEquals("Canada", scoreBoard.getAwayTeam());
+        Assert.assertEquals(0, scoreBoard.getHomeTeamScore());
+        Assert.assertEquals(0, scoreBoard.getAwayTeamScore());
     }
 
     @Test
     public void testFinishGame() {
         //Given
-        FootballWorldCupScoreBoard footballWorldCupScoreBoard = new FootballWorldCupScoreBoard();
+        final ScoreBoard scoreBoard = new ScoreBoard();
+        FootballWorldCupScoreBoard footballWorldCupScoreBoard = new FootballWorldCupScoreBoard(scoreBoard);
 
         //When
+        footballWorldCupScoreBoard.startGame("Mexico ", " Canada ");
         footballWorldCupScoreBoard.finishGame();
 
         //Then
         Assert.assertFalse(footballWorldCupScoreBoard.isGameContinues());
+        Assert.assertNull(scoreBoard.getHomeTeam());
+        Assert.assertNull(scoreBoard.getAwayTeam());
     }
 
     @Test
     public void testUpdateScore() {
         //Given
-        FootballWorldCupScoreBoard footballWorldCupScoreBoard = new FootballWorldCupScoreBoard();
+        final ScoreBoard scoreBoard = new ScoreBoard();
+        FootballWorldCupScoreBoard footballWorldCupScoreBoard = new FootballWorldCupScoreBoard(scoreBoard);
 
         //When
         footballWorldCupScoreBoard.startGame("Mexico", "Canada");
@@ -64,7 +75,8 @@ public class FootballWorldCupScoreBoardTestSuite {
     @Test
     public void testGetSummaryOfGamesByTotalScore() {
         //Given
-        FootballWorldCupScoreBoard footballWorldCupScoreBoard = new FootballWorldCupScoreBoard();
+        final ScoreBoard scoreBoard = new ScoreBoard();
+        FootballWorldCupScoreBoard footballWorldCupScoreBoard = new FootballWorldCupScoreBoard(scoreBoard);
 
         //When
         int summaryOfGamesScoreSize = footballWorldCupScoreBoard.getSummaryOfGamesByTotalScore().size();
